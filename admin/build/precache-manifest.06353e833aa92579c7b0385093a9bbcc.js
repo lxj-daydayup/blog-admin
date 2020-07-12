e8f8bd5ce815a44443db76dc1058f2ed6bc45ba4
lxj-daydayup <65936722+lxj-daydayup@users.noreply.github.com>
2020-07-12 12:30:20 +0800

    添加后台

---
+++ admin/build/precache-manifest.06353e833aa92579c7b0385093a9bbcc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9379e5d4a1110379b9b4aab4b44c999f",
+    "url": "/start/index.html"
+  },
+  {
+    "revision": "c71288b213d351882c12",
+    "url": "/start/static/css/2.40eb9442.chunk.css"
+  },
+  {
+    "revision": "198011ee2ddf0ec49178",
+    "url": "/start/static/css/main.bc182ab8.chunk.css"
+  },
+  {
+    "revision": "c71288b213d351882c12",
+    "url": "/start/static/js/2.d8062a8f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/start/static/js/2.d8062a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "198011ee2ddf0ec49178",
+    "url": "/start/static/js/main.cdb68aef.chunk.js"
+  },
+  {
+    "revision": "90e8b8e0095597f89450",
+    "url": "/start/static/js/runtime-main.1ef7d531.js"
+  }
+]);

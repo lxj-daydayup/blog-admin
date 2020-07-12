bedf67e043ea44079099b221e8743a8ee3a8f66b
lxj-daydayup <65936722+lxj-daydayup@users.noreply.github.com>
2020-07-12 12:42:49 +0800

    build

---
+++ build/src/config/apiURL.js
@@ -0,0 +1,14 @@
+let ip='http://175.24.15.197:7001/admin'
+let servicePath={
+    checkLogin:ip+'/checkLogin',
+    getTypeInfo:ip+'/getTypeInfo',
+    addArticle:ip+'/addArticle',
+    changeArticle:ip+'/changeArticle',
+    getArticleList:ip+'/getArticleList',
+    delArticle:ip+'/delArticle/',
+    getArticleById:ip+'/getArticleById/'
+
+    
+}
+;
+export default servicePath;

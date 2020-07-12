7f241d98fdafe5a9a6bc1f330baf4ba5118c6330
lxj-daydayup <65936722+lxj-daydayup@users.noreply.github.com>
2020-07-12 13:06:25 +0800

    Updates

---
+++ precache-manifest.aeafb2083abf9ab3011975d8de90cb36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0df718295880711cc56cd53a2451673d",
+    "url": "/start/index.html"
+  },
+  {
+    "revision": "4250a6114a1d9510ac26",
+    "url": "/start/static/css/2.40eb9442.chunk.css"
+  },
+  {
+    "revision": "2cc04764d6c7642f8037",
+    "url": "/start/static/css/main.bc182ab8.chunk.css"
+  },
+  {
+    "revision": "4250a6114a1d9510ac26",
+    "url": "/start/static/js/2.35ca094f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/start/static/js/2.35ca094f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cc04764d6c7642f8037",
+    "url": "/start/static/js/main.1795c04d.chunk.js"
+  },
+  {
+    "revision": "90e8b8e0095597f89450",
+    "url": "/start/static/js/runtime-main.1ef7d531.js"
+  }
+]);
